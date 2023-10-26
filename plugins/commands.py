52e2336480185619d953911b89f2cd0552d4c81d
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-10-27 01:36:55 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -145,7 +145,7 @@ async def start(client, message):
                 f_caption = f"{title}"
             try:
                 await client.send_cached_media(
-                    chat_id=info.TO_CHANNEL,
+                    chat_id=TO_CHANNEL,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
@@ -154,7 +154,7 @@ async def start(client, message):
                 await asyncio.sleep(e.x)
                 logger.warning(f"Floodwait of {e.x} sec.")
                 await client.send_cached_media(
-                    chat_id=info.TO_CHANNEL,
+                    chat_id=TO_CHANNEL,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
                     protect_content=msg.get('protect', False),
