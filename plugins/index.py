6bee5ab75c6b4e72fd41fd6a8eb9e2936675ecf7
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-10-28 14:01:42 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -155,10 +155,10 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
                     can = [[InlineKeyboardButton('Cancel', callback_data='index_cancel')]]
                     reply = InlineKeyboardMarkup(can)
                     try:
-                        await msg.edit_text(text=f"Total messages fetched: <code>{current}</code>\nTotal messages saved: <code>{total_files}</code>\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>\nChat ID/ Username - <code> {chat_id}</code>\n\n𝐍𝐚𝐦𝐞 : [{ BOTNAME}](tg://user?id={BOTID})", reply_markup=reply)       
+                        await msg.edit_text(text=f"Total messages fetched: <code>{current}</code>\nTotal messages saved: <code>{total_files}</code>\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>\n\n𝐍𝐚𝐦𝐞 : [{ BOTNAME}](tg://user?id={BOTID})", reply_markup=reply)       
                     except FloodWait as t:
                         await asyncio.sleep(t.value)
-                        await msg.edit_text(text=f"Total messages fetched: <code>{current}</code>\nTotal messages saved: <code>{total_files}</code>\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>\nChat ID/ Username - <code> {chat_id}</code>\n\n𝐍𝐚𝐦𝐞 : [{ BOTNAME}](tg://user?id={BOTID})", reply_markup=reply)                          
+                        await msg.edit_text(text=f"Total messages fetched: <code>{current}</code>\nTotal messages saved: <code>{total_files}</code>\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>\n\n𝐍𝐚𝐦𝐞 : [{ BOTNAME}](tg://user?id={BOTID})", reply_markup=reply)                          
                 if message.empty:
                     deleted += 1
                     continue
@@ -185,7 +185,7 @@ async def index_files_to_db(lst_msg_id, chat, msg, bot):
             logger.exception(e)
             await msg.edit(f'Error: {e}')
         else:
-            await msg.edit(f'Succesfully saved <code>{total_files}</code> to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>\nChat ID/ Username - <code> {chat_id}</code>\n\n𝐍𝐚𝐦𝐞 : [{ BOTNAME}](tg://user?id={BOTID})')
+            await msg.edit(f'Succesfully saved <code>{total_files}</code> to dataBase!\nDuplicate Files Skipped: <code>{duplicate}</code>\nDeleted Messages Skipped: <code>{deleted}</code>\nNon-Media messages skipped: <code>{no_media + unsupported}</code>(Unsupported Media - `{unsupported}` )\nErrors Occurred: <code>{errors}</code>\n\n𝐍𝐚𝐦𝐞 : [{ BOTNAME}](tg://user?id={BOTID})')
 
 
 
