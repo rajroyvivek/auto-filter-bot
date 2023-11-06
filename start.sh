db0c740e0c1cfc893b3692a71480022a7a19f322
rajroyvivek <149822657+rajroyvivek@users.noreply.github.com>
2023-11-06 21:35:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Unknownforall1/Auto-search-tamil-bot.git /Auto-search-tamil-bot
+  git clone https://github.com/rejroyvivek/Auto-filter-bot.git /Auto-filter-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto-filter-bot
