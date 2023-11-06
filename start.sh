1b80814f0e72a354f60fb77b68c5d50790c643ba
rajroyvivek <149822657+rajroyvivek@users.noreply.github.com>
2023-11-06 21:30:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git clone https://github.com/Unknownforall1/Auto-search-tamil-bot.git /Auto-search-tamil-bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Auto-search-tamil-bot
+  git clone $UPSTREAM_REPO /Auto-filter-bot
 fi
-cd /Auto-search-tamil-bot
+cd /Auto-filter-bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
